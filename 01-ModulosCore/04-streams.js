204a35a5af8d25c1b1f5969a52d745410f6c143a
Gary Guzman <gary.2810.dav@gmail.com>
2018-08-12 21:03:25 -0400

    super chat

--- 01-ModulosCore/04-streams.js
+++ 01-ModulosCore/04-streams.js
@@ -1,3 +1,18 @@
+/*
+Streams
+	'Chorros' de información que se transmiten en 'Pedazos' (Chunks)
+	3 tipos: Lectura / Escritura / Duplex
+	Instancias de EventEmitter
+	Acceso asíncrono
+	Es raro crear streams directamente
+	Pero muchos recursos nos ofrecen este Interfaz
+	Detrás de muchos mecanismos de Node.JS
+		stdin/stdout
+		request de HTTP
+		Sockets
+		Manipulación de ficheros/imágenes
+*/
+
 'use strict'
 
 var fs = require('fs'),
